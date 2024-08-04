c9bc82cbdac4240c12e68e861dc4e9e671a48d3a
Konstantin Stefanov <117399227+Elsaxos@users.noreply.github.com>
2024-08-04 18:07:10 +0300

    Add project files.

---
+++ AppiumDemoProject/SummatorAppTest.cs
@@ -0,0 +1,58 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+
+namespace AppiumDemoProject
+{
+    public class SummatorAppTest
+    {
+        private AndroidDriver _driver;
+
+        private AppiumLocalService _appiumLocalService;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _appiumLocalService = new AppiumServiceBuilder()
+                .WithIPAddress("127.0.0.1")
+                .UsingPort(4723)
+                .Build();
+            _appiumLocalService.Start();
+
+            var androidOptions = new AppiumOptions
+            {
+                PlatformName = "Android",
+                AutomationName = "UiAutomator2",
+                DeviceName = "AppiumTestPhone",
+                App = @"C:\\Users\\User\\Desktop\\com.example.androidappsummator.apk",
+                PlatformVersion = "13"
+
+            };
+            _driver = new AndroidDriver(_appiumLocalService, androidOptions);
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        { 
+            _driver.Quit();
+            _driver.Dispose();
+            _appiumLocalService?.Dispose();
+        
+        }
+
+        [Test]
+        public void TestWithValidData()
+        {
+            IWebElement field1 = _driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/editText1"));
+            field1.Clear();
+            field1.SendKeys("1");
+            IWebElement field2 = _driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/editText2"));
+            field2.Clear();
+            field2.SendKeys("2");
+            IWebElement calcButton = _driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/buttonCalcSum"));
+            calcButton.Click();
+            IWebElement resultField = _driver.FindElement(MobileBy.Id("com.example.androidappsummator:id/editTextSum"));
+
+            Assert.That(resultField.Text, Is.EqualTo("3"));
+        }
+    }
+}
